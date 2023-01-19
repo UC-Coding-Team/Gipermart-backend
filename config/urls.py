7d4006c8e63bc7e8cc575c432fbca3676f2a93c8
Abdulla Abdukulov <abduqulovabdulla3108@gmail.com>
2023-01-19 18:25:22 +0500

    changed by Abdullah

--- config/urls.py
+++ config/urls.py
@@ -17,29 +17,21 @@ from django.contrib import admin
 from django.urls import path, re_path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import path,include
+from django.urls import path, include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    re_path(r'^ckeditor/', include('ckeditor_uploader.urls')),
-
-
-
 schema_view = get_schema_view(
-   openapi.Info(
-      title="Giermart API",
-      default_version='v1',
-      description="Documentation Gipermart",
-      license=openapi.License(name="BSD License"),
-   ),
-   public=True,
-   permission_classes=[permissions.AllowAny],
+    openapi.Info(
+        title="Giermart API",
+        default_version='v1',
+        description="Documentation Gipermart",
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
 )
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -47,7 +39,7 @@ urlpatterns = [
     path("user/", include("apps.user_profil.urls")),
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-
+    re_path(r'^ckeditor/', include('ckeditor_uploader.urls')),
 ]
 
 if settings.DEBUG:
