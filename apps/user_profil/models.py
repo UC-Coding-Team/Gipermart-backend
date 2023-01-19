df8a882ca7349e06589236a8e77589c41e2cf251
student <jasurabdibayev0@gmail.com>
2023-01-19 19:36:28 +0500

    all

--- apps/user_profil/models.py
+++ apps/user_profil/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import (
-    BaseUserManager, AbstractBaseUser
+    BaseUserManager, AbstractBaseUser, PermissionsMixin
 )
 
 # Custom User Maneger.
@@ -39,7 +39,7 @@ class UserManager(BaseUserManager):
 
 
 # Custom User Model.
-class User(AbstractBaseUser):
+class User(AbstractBaseUser,PermissionsMixin):
     email = models.EmailField(
         verbose_name='Email',
         max_length=255,
@@ -61,18 +61,12 @@ class User(AbstractBaseUser):
         return self.email
 
     def has_perm(self, perm, obj=None):
-        "Does the user have a specific permission?"
-        # Simplest possible answer: Yes, always
-        return self.is_admin
+        return self.is_superuser
 
     def has_module_perms(self, app_label):
-        "Does the user have permissions to view the app `app_label`?"
-        # Simplest possible answer: Yes, always
-        return True
+        return self.is_superuser
 
     @property
     def is_staff(self):
-        "Is the user a member of staff?"
-        # Simplest possible answer: All admins are staff
         return self.is_admin
 
