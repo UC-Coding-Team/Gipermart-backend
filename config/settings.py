e06404a0b703a7536ca2a0d6fdad6914eb84bc69
student <jasurabdibayev0@gmail.com>
2023-01-21 18:01:05 +0500

    Adminlite3 Changed by Jasur

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,11 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'jazzmin',
+    # General use templates & template tags (should appear first)
+    'adminlte3',
+    # Optional: Django admin theme (must be before django.contrib.admin)
+    'adminlte3_theme',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
