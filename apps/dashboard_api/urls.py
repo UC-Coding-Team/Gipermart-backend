3f7514c4379a697295f6286d848889e168054fb0
Abdulla Abdukulov <abduqulovabdulla3108@gmail.com>
2023-01-25 13:32:09 +0500

    dashboard updated  changed by Abdullah

--- apps/dashboard_api/urls.py
+++ apps/dashboard_api/urls.py
@@ -5,6 +5,11 @@ from . import views
 router = DefaultRouter()
 
 router.register(r'categories', views.CategoryViewSet, basename='category')
+router.register(r'products', views.ProductViewSet, basename='product')
+router.register(r'colors', views.ColorViewSet, basename='color')
+router.register(r'sizes', views.SizeViewSet, basename='size')
+router.register(r'variants', views.VariantViewSet, basename='variant')
+
 
 urlpatterns = [
     path('', include(router.urls)),
--- apps/dashboard_api/views.py
+++ apps/dashboard_api/views.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets
-from .serializers import CategorySerializers
-from apps.products.models import Category
+from .serializers import CategorySerializers, ProductSerializers, ColorSerializers, SizeSerializers, VariantSerializers
+from apps.products.models import Category, Product, Color, Size, Variants
 from rest_framework.permissions import IsAuthenticated
 
 
@@ -9,3 +9,26 @@ class CategoryViewSet(viewsets.ModelViewSet):
     serializer_class = CategorySerializers
     permission_classes = [IsAuthenticated]
 
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializers
+    permission_classes = [IsAuthenticated]
+
+
+class ColorViewSet(viewsets.ModelViewSet):
+    queryset = Color.objects.all()
+    serializer_class = ColorSerializers
+    permission_classes = [IsAuthenticated]
+
+
+class SizeViewSet(viewsets.ModelViewSet):
+    queryset = Size.objects.all()
+    serializer_class = ColorSerializers
+    permission_classes = [IsAuthenticated]
+
+
+class VariantViewSet(viewsets.ModelViewSet):
+    queryset = Variants.objects.all()
+    serializer_class = VariantSerializers
+    permission_classes = [IsAuthenticated]
