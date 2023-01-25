2d04f742755e8fbe709643085adc574e161c3faa
student <jasurabdibayev0@gmail.com>
2023-01-25 17:37:28 +0500

    all api

--- apps/outside/urls.py
+++ apps/outside/urls.py
@@ -3,13 +3,16 @@ from .views import Sliderviews,Stockviews,Brandviews
 # from
 from rest_framework.routers import DefaultRouter
 
+router = DefaultRouter()
+
+router.register(r"slider", Sliderviews)
+router.register(r"stock", Stockviews)
+router.register(r"brand", Brandviews)
+
 
 
 urlpatterns = [
-    # path("", include(router.urls)),
-    path("slider/", Sliderviews.as_view(),name="Sliderviews"),
-    path("stock/", Stockviews.as_view(), name="Stockviews"),
-    path("brand/", Brandviews.as_view(), name="Brandviews"),
+    path("", include(router.urls)),
     # path("Add_to_cart/", Add_to_cartviews.as_view(), name="Add_to_cartviews"),
 
 ]
--- apps/outside/views.py
+++ apps/outside/views.py
@@ -1,33 +1,29 @@
-from rest_framework.response import Response
-from rest_framework.views import APIView
-
+from rest_framework import viewsets
 from .models import Slider,Stock,Brand
 from .serializers import Slider_serializer,Stock_serializer,Brand_serializer
+from ..products.permission import ModelViewSetsPermission
 
 
-class Sliderviews(APIView):
+class Sliderviews(viewsets.ModelViewSet):
+    permission_classes = (ModelViewSetsPermission,)
     serializer_class = Slider_serializer
+    queryset = Slider.objects.all()
+    http_method_names = ("get",)
 
-    def get(self, request, format=None):
-        transformers = Slider.objects.all()
-        serializer = Slider_serializer(transformers, many=True)
-        return Response(serializer.data)
 
-class Stockviews(APIView):
+class Stockviews(viewsets.ModelViewSet):
+    permission_classes = (ModelViewSetsPermission,)
     serializer_class = Stock_serializer
+    queryset = Stock.objects.all()
+    http_method_names = ("get",)
 
-    def get(self, request, format=None):
-        transformers = Stock.objects.all()
-        serializer = Stock_serializer(transformers, many=True)
-        return Response(serializer.data)
 
-class Brandviews(APIView):
-    serializer_class = Stock_serializer
+class Brandviews(viewsets.ModelViewSet):
+    permission_classes = (ModelViewSetsPermission,)
+    serializer_class = Brand_serializer
+    queryset = Brand.objects.all()
+    http_method_names = ("get",)
 
-    def get(self, request, format=None):
-        transformers = Brand.objects.all()
-        serializer = Brand_serializer(transformers, many=True)
-        return Response(serializer.data)
 
 # class Add_to_cartviews(APIView):
 #     serializer_class = Add_to_cart_serializer
