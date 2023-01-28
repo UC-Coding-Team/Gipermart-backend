d2967c1570036c655053d0442d540550072ad9f0
Abdulla Abdukulov <abduqulovabdulla3108@gmail.com>
2023-01-28 12:46:49 +0500

    changed by Abdulla

--- apps/products/admin.py
+++ apps/products/admin.py
@@ -2,12 +2,32 @@ from django.contrib import admin
 
 from . import models
 
+
 admin.site.register(models.Product)
 admin.site.register(models.Category)
+admin.site.register(models.ProductAttribute)
+admin.site.register(models.ProductAttributeValue)
+admin.site.register(models.ProductAttributeValues)
 
 
+# class ProductAttributeAdmin(admin.TabularInline):
+#     model = models.ProductAttribute
+#     raw_id_fields = ['product']
+#
+#
+# class ProductAttributeValueAdmin(admin.TabularInline):
+#     model = models.ProductAttributeValue
+#     raw_id_fields = ['product']
+#
+#
+# class ProductAttributeValuesAdmin(admin.TabularInline):
+#     model = models.ProductAttributeValues
+#     raw_id_fields = ['product']
+
+
+@admin.register(models.ProductInventory)
 class InventoryAdmin(admin.ModelAdmin):
     list_display = ("product", "store_price")
+    # inlines = [ProductAttributeAdmin, ProductAttributeValueAdmin, ProductAttributeValuesAdmin]
 
-
-admin.site.register(models.ProductInventory, InventoryAdmin)
+# admin.site.register(models.ProductInventory, InventoryAdmin)
