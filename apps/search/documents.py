098032f88639e3542b3918d5817d6afcf5db56ec
jasur <jasurabdibayev0@gmail.com>
2023-01-29 20:12:20 +0500

    add

--- apps/search/documents.py
+++ apps/search/documents.py
@@ -2,14 +2,34 @@ from django_elasticsearch_dsl import Document, fields
 from django_elasticsearch_dsl.registries import registry
 from apps.products.models import ProductInventory
 
-
 @registry.register_document
 class ProductInventoryDocument(Document):
-
     product = fields.ObjectField(
-        properties={"name": fields.TextField(), "id": fields.IntegerField()}
-    )
+        properties={
+            "name": fields.TextField(),
+            "slug": fields.TextField(),
+            "description": fields.TextField(),
+        #     "category": fields.ObjectField(
+        # properties={
+        # "name": fields.TextField(),
+        # "slug": fields.TextField(),
+        # "description": fields.TextField(),
+        # "background_image": fields.FileField(),
+        # }
+    # )
+}
+)
     brand = fields.ObjectField(properties={"name": fields.TextField()})
+    product_type = fields.ObjectField(properties={"name": fields.TextField(),"slug": fields.TextField(),"description": fields.TextField()})
+    attribute_values = fields.ObjectField(
+        properties={
+        "name": fields.TextField(),
+        "value": fields.TextField(),
+        "slug": fields.TextField(),
+        "description": fields.TextField(),
+        "background_image": fields.FileField(),
+    }
+)
 
     class Index:
         name = "productinventory"
@@ -20,6 +40,13 @@ class ProductInventoryDocument(Document):
         fields = [
             "id",
             "sku",
+            "upc",
             "price",
             "is_default",
+            "is_active",
+            "is_on_sale",
+            "sale_price",
+            "weight",
+            "created_at",
+            "updated_at",
         ]
