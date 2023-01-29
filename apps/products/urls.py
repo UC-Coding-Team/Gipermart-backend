2178fef03ba2ca5fb0a670da6d434529affb00ad
Abdulla Abdukulov <abduqulovabdulla3108@gmail.com>
2023-01-29 18:04:25 +0500

    changed by Abdulla

--- apps/products/urls.py
+++ apps/products/urls.py
@@ -1,18 +1,3 @@
-# from django.urls import path, include
-# from rest_framework.routers import DefaultRouter
-# from . import views
-#
-# router = DefaultRouter()
-#
-# router.register(r'wishlists', views.WishlistViewSet)
-# router.register(r'categories', views.CategoryViewSet)
-# router.register(r'products', views.ProductViewSet)
-#
-# urlpatterns = [
-#     path("", include(router.urls)),
-# ]
-
-
 from django.urls import path
 
 from apps.products.views import CategoryList, ProductByCategory, ProductInventoryById
--- apps/products/views.py
+++ apps/products/views.py
@@ -1,25 +1,3 @@
-# from rest_framework import viewsets
-# from .serializers import CategorySerializer, ProductSerializer, WishlistSerializer
-# from .models import Category, Product, Wishlist
-
-
-# class CategoryViewSet(viewsets.ModelViewSet):
-#     queryset = Category.objects.all()
-#     serializer_class = CategorySerializer
-#     http_method_names = ("get",)
-#
-#
-# class ProductViewSet(viewsets.ModelViewSet):
-#     queryset = Product.objects.all()
-#     serializer_class = ProductSerializer
-#     http_method_names = ("get",)
-#
-#
-# class WishlistViewSet(viewsets.ModelViewSet):
-#     queryset = Wishlist.objects.all()
-#     serializer_class = WishlistSerializer
-
-
 from .serializers import (
     CategorySerializer,
     ProductInventorySerializer,
