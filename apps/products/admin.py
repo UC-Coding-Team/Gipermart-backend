728d9c3a40e40694507547a3931cd054a80e5ba4
Abdulla Abdukulov <abduqulovabdulla3108@gmail.com>
2023-02-03 17:42:55 +0500

    changed by Abdulla

--- apps/products/admin.py
+++ apps/products/admin.py
@@ -19,12 +19,12 @@ class ProductMediaInline(admin.TabularInline):
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['name', 'slug', 'category', 'is_active', 'is_recommended', 'USA_product', 'created_at', 'updated_at']
+    list_display = ['name', 'slug', 'category', 'is_active', 'is_recommended', 'status', 'USA_product', 'created_at', 'updated_at']
     prepopulated_fields = {'slug': ('name',)}
     inlines = [ProductInventoryInline, ]
     list_filter = ['is_active', 'created_at', 'updated_at']
     list_editable = ['is_active', 'is_recommended', 'USA_product']
-    fields = ('name', 'slug', 'description', 'category', 'is_active', 'is_recommended', 'USA_product')
+    fields = ('name', 'slug', 'description', 'category', 'is_active', 'status', 'is_recommended', 'USA_product')
 
 
 @admin.register(models.Category)
