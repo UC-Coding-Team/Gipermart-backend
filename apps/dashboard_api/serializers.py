c16f52c2e7df79bea780a276cc89308a4b08d4ea
Abdulla Abdukulov <abduqulovabdulla3108@gmail.com>
2023-02-07 12:39:10 +0500

    changed by Abdullah

--- apps/dashboard_api/serializers.py
+++ apps/dashboard_api/serializers.py
@@ -1,6 +1,66 @@
 from rest_framework import serializers
-from apps.products.models import Category, Product
-from apps.outside.models import Slider, Stock, Brand
+from apps.products.models import (
+    Category, Product, Brand,
+    ProductAttribute, ProductType,
+    ProductAttributeValue, ProductInventory,
+    Media, Stock, ProductAttributeValues, ProductTypeAttribute,
+    Wishlist,
+)
+from apps.outside import models
+
+
+class ProductTypeSerializers(serializers.ModelSerializer):
+    class Meta:
+        models = ProductType
+        fields = '__all__'
+
+
+class ProductTypeAttributeSerializers(serializers.ModelSerializer):
+    class Meta:
+        models = ProductTypeAttribute
+        fields = '__all__'
+
+
+class WishlistSerializers(serializers.ModelSerializer):
+    class Meta:
+        models = Wishlist
+        fields = '__all__'
+
+
+class ProductAttributeValueSerializers(serializers.ModelSerializer):
+    class Meta:
+        models = ProductAttributeValue
+        fields = '__all__'
+
+
+class ProductInventorySerializers(serializers.ModelSerializer):
+    class Meta:
+        models = ProductInventory
+        fields = '__all__'
+
+
+class MediaSerializers(serializers.ModelSerializer):
+    class Meta:
+        models = Media
+        fields = '__all__'
+
+
+class ProductAttributeSerializers(serializers.ModelSerializer):
+    class Meta:
+        models = ProductAttribute
+        fields = '__all__'
+
+
+class StockProductSerializers(serializers.ModelSerializer):
+    class Meta:
+        models = Stock
+        fields = '__all__'
+
+
+class ProductAttributeValuesSerializers(serializers.ModelSerializer):
+    class Meta:
+        models = ProductAttributeValues
+        fields = '__all__'
 
 
 class CategorySerializers(serializers.ModelSerializer):
@@ -17,13 +77,13 @@ class ProductSerializers(serializers.ModelSerializer):
 
 class SliderSerializers(serializers.ModelSerializer):
     class Meta:
-        model = Slider
+        model = models.Slider
         fields = ('id', 'slug', 'images', 'created_at', 'updated_at')
 
 
 class StockSerializers(serializers.ModelSerializer):
     class Meta:
-        model = Stock
+        model = models.Stock
         fields = ('id', 'slug', 'images', 'created_at', 'updated_at')
 
 
@@ -31,5 +91,3 @@ class BrandSerializers(serializers.ModelSerializer):
     class Meta:
         model = Brand
         fields = ('id', 'slug', 'images', 'category', 'product', 'created_at', 'updated_at')
-
-
