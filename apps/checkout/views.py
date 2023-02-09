0c5cfac744fc3754e93404634af26325b2a39f1b
FathullaLatipov <fathullaxonlatipov@gmail.com>
2023-02-09 16:31:08 +0500

    new jwt settings

--- apps/checkout/views.py
+++ apps/checkout/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from rest_framework import generics
 from .models import Checkout
-from .serializers import CheckoutSerializer,CheckoutAllSerializer
+from .serializers import CheckoutSerializer, CheckoutAllSerializer
 
-class CheckoutList(generics.ListCreateAPIView):
+
+class CheckoutList(generics.CreateAPIView):
     queryset = Checkout.objects.all()
     serializer_class = CheckoutSerializer
+
 
 class CheckoutDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Checkout.objects.all()
