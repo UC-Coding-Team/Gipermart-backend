b21a0842dd01c03d7040d35706a453e8a0465130
Abdulla Abdukulov <abduqulovabdulla3108@gmail.com>
2023-02-24 18:07:51 +0500

    updated

--- apps/dashboard_api/admin.py
+++ apps/dashboard_api/admin.py
@@ -2,4 +2,6 @@ from django.contrib import admin
 from .models import SiteSettings
 
 
-admin.register(SiteSettings)
+@admin.register(SiteSettings)
+class SiteSettingsAdmin(admin.ModelAdmin):
+    list_display = ['id', 'logo', 'phonenumbers', 'site_type', 'description']
--- apps/dashboard_api/models.py
+++ apps/dashboard_api/models.py
@@ -1,15 +1,16 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
 class SiteSettings(models.Model):
-    logo = models.ImageField(upload_to='logos', null=True, blank=True)
-    phonenumbers = models.CharField(max_length=20, blank=True, null=True)
-    site_type = models.CharField(max_length=100, null=True, blank=True)
-    description = models.CharField(max_length=200, null=True, blank=True)
-    instagram = models.URLField(null=True, blank=True)
-    facebook = models.URLField(null=True, blank=True)
-    telegram = models.URLField(null=True, blank=True)
-    youtube = models.URLField(null=True, blank=True)
+    logo = models.ImageField(upload_to='logos', null=True, blank=True, verbose_name=_('logo'))
+    phonenumbers = models.CharField(max_length=20, blank=True, null=True, verbose_name=_('phone number'))
+    site_type = models.CharField(max_length=100, null=True, blank=True, verbose_name=_('site type'))
+    description = models.CharField(max_length=200, null=True, blank=True, verbose_name=_('description'))
+    instagram = models.URLField(null=True, blank=True, verbose_name=_('instagram link'))
+    facebook = models.URLField(null=True, blank=True, verbose_name=_('facebook link'))
+    telegram = models.URLField(null=True, blank=True, verbose_name=_('telegram link'))
+    youtube = models.URLField(null=True, blank=True, verbose_name=_('youtube link'))
 
     def __str__(self):
         return self.site_type
