504a77caae93bc82b8a33d09de7dccf0798ed35f
FathullaLatipov <fathullaxonlatipov@gmail.com>
2023-03-05 00:38:46 +0500

    new filter

--- apps/products/filter.py
+++ apps/products/filter.py
@@ -6,10 +6,12 @@ from apps.products.models import ProductInventory
 class ProductFilter(django_filters.FilterSet):
     name__not = django_filters.CharFilter(field_name='attribute_values', exclude=True)
     name__not__in = django_filters.CharFilter(field_name='attribute_values', method='filter_name_not_in')
+    min_price = django_filters.NumberFilter(field_name='price', lookup_expr='gte')
+    max_price = django_filters.NumberFilter(field_name='price', lookup_expr='lte')
 
     class Meta:
         model = ProductInventory
-        fields = ['attribute_values']
+        fields = ['attribute_values', 'price']
 
     @staticmethod
     def filter_name_not_in(queryset, _, value):
