66d8db559da5a392f790ff248ad9b863b5da6e27
FathullaLatipov <fathullaxonlatipov@gmail.com>
2023-03-06 18:45:05 +0500

    new dashboard

--- apps/dashboard_api/urls.py
+++ apps/dashboard_api/urls.py
@@ -1,35 +1,15 @@
 from rest_framework.routers import DefaultRouter
 from django.urls import path, include
 from . import views
-from .views import selling_status, LoginView, ChangePasswordView, ForgetPasswordView
 
 router = DefaultRouter()
 
-router.register(r'product-stocks', views.ProductStockViewSet, basename='product-stock')
-router.register(r'product-attribute-values', views.ProductAttributeValuesViewSet, basename='product-attribute-values')
-router.register(r'product-attribute-value', views.ProductAttributeValueViewSet, basename='product-attribute-value')
-router.register(r'wish-lists', views.WishlistViewSet, basename='wish-list')
-router.register(r'product-inventors', views.ProductInventoryViewSet, basename='product-inventory')
-router.register(r'product-media', views.MediaViewSet, basename='media')
-router.register(r'product-attribute', views.ProductAttributeViewSet, basename='product-attribute')
-router.register(r'product-type', views.ProductTypeViewSet, basename='product-type')
-router.register(r'product-brand', views.BrandProductViewSet, basename='product-brand')
-router.register(r'product-type-attribute', views.ProductTypeAttributeViewSet, basename='product-type-attribute')
-router.register(r'categories', views.CategoryViewSet, basename='category')
-router.register(r'products', views.ProductViewSet, basename='product')
+
 router.register(r'sliders', views.SliderViewSet, basename='slider')
 router.register(r'stocks', views.StockViewSet, basename='stock')
 router.register(r'brands', views.BrandViewSet, basename='brand')
-router.register(r'users', views.UsersViewSet, basename='user')
-router.register(r'checkout', views.CheckoutViewSet, basename='checkout')
-router.register(r'site-settings', views.SiteSettingsViewSet, basename='site-settings')
-router.register(r'phone-site-settings', views.PhoneSiteSettingsViewSet, basename='site-settings')
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('selling-count/', selling_status, name='selling-count'),
-    path('login/', LoginView.as_view(), name='login'),
-    path('change-password/', ChangePasswordView.as_view(), name='change-password'),
-    path('forget-password/', ForgetPasswordView.as_view(), name='forget-password'),
 ]
 
--- apps/dashboard_api/views.py
+++ apps/dashboard_api/views.py
@@ -1,121 +1,14 @@
-from django.db.models import Sum, Q, DecimalField
-from django.db.models.functions import Cast
-from django.utils import timezone
 from rest_framework import viewsets
-from rest_framework.decorators import api_view
-from rest_framework.filters import SearchFilter
-from rest_framework.views import APIView
-from rest_framework.permissions import IsAuthenticated, IsAdminUser
-from rest_framework.response import Response
-from rest_framework_simplejwt.views import TokenObtainPairView
-from django.core.mail import send_mail
-from django.conf import settings
-from django.utils.translation import gettext_lazy as _
 from django.contrib.auth import get_user_model
-from .serializers import LoginSerializer, ChangePasswordSerializer, ForgetPasswordSerializer
+from rest_framework.filters import SearchFilter
+
 from . import serializers as serial
-from apps.products.models import (
-    Category, Product, Brand,
-    ProductAttribute, ProductType,
-    ProductAttributeValue, ProductInventory,
-    Media, Stock, ProductAttributeValues, ProductTypeAttribute,
-    Wishlist,
-)
-from rest_framework import serializers
 from .models import SiteSettings
 from rest_framework.permissions import AllowAny
 from apps.outside import models
-from .serializers import SiteSettingsSerializers, PhoneSiteSettingsSerializers, \
-    SellingStatusSerializer
-from ..cart.serializers import DashUserSerializer
-from ..checkout.models import Checkout
-from ..checkout.serializers import CheckoutSerializer
+from .serializers import SiteSettingsSerializers, PhoneSiteSettingsSerializers
 
 User = get_user_model()
-
-
-class ProductStockViewSet(viewsets.ModelViewSet):
-    queryset = Stock.objects.all()
-    serializer_class = serial.StockProductSerializers
-    permission_classes = [AllowAny]
-
-
-class ProductAttributeValuesViewSet(viewsets.ModelViewSet):
-    queryset = ProductAttributeValues.objects.all()
-    serializer_class = serial.ProductAttributeValuesSerializers
-    permission_classes = [AllowAny]
-
-
-class ProductTypeAttributeViewSet(viewsets.ModelViewSet):
-    queryset = ProductTypeAttribute.objects.all()
-    serializer_class = serial.ProductTypeAttributeSerializers
-    permission_classes = [AllowAny]
-
-
-class ProductAttributeValueViewSet(viewsets.ModelViewSet):
-    queryset = ProductAttributeValue.objects.all()
-    serializer_class = serial.ProductAttributeValueSerializers
-    permission_classes = [AllowAny]
-
-
-class WishlistViewSet(viewsets.ModelViewSet):
-    queryset = Wishlist.objects.all()
-    serializer_class = serial.WishlistSerializers
-    permission_classes = [AllowAny]
-
-
-class ProductInventoryViewSet(viewsets.ModelViewSet):
-    queryset = ProductInventory.objects.all()
-    serializer_class = serial.ProductInventorySerializers
-    permission_classes = [AllowAny]
-    filter_backends = [SearchFilter]
-    search_fields = ['sku', ]
-
-
-class MediaViewSet(viewsets.ModelViewSet):
-    queryset = Media.objects.all()
-    serializer_class = serial.MediaSerializers
-    permission_classes = [AllowAny]
-
-
-class ProductAttributeViewSet(viewsets.ModelViewSet):
-    queryset = ProductAttribute.objects.all()
-    serializer_class = serial.ProductAttributeSerializers
-    permission_classes = [AllowAny]
-    filter_backends = [SearchFilter]
-    search_fields = ['id', 'name']
-
-
-class ProductTypeViewSet(viewsets.ModelViewSet):
-    queryset = ProductType.objects.all()
-    serializer_class = serial.ProductTypeSerializers
-    permission_classes = [AllowAny]
-    filter_backends = [SearchFilter]
-    search_fields = ['id', 'name']
-
-
-class BrandProductViewSet(viewsets.ModelViewSet):
-    queryset = Brand.objects.all()
-    serializer_class = serial.ProductBrandSerializers
-    permission_classes = [AllowAny]
-    filter_backends = [SearchFilter]
-    search_fields = ['id', 'name']
-
-
-class CategoryViewSet(viewsets.ModelViewSet):
-    queryset = Category.objects.all()
-    serializer_class = serial.CategorySerializers
-    permission_classes = [AllowAny]
-    filter_backends = [SearchFilter]
-    search_fields = ['id', 'name']
-
-
-class ProductViewSet(viewsets.ModelViewSet):
-    queryset = Product.objects.all()
-    serializer_class = serial.ProductSerializers
-    permission_classes = [AllowAny]
-    filter_backends = [SearchFilter]
-    search_fields = ['id', 'name']
 
 
 class SliderViewSet(viewsets.ModelViewSet):
@@ -142,22 +35,6 @@ class BrandViewSet(viewsets.ModelViewSet):
     search_fields = ['id', 'slug']
 
 
-class UsersViewSet(viewsets.ModelViewSet):
-    queryset = models.User.objects.all()
-    serializer_class = DashUserSerializer
-    permission_classes = [AllowAny]
-    filter_backends = [SearchFilter]
-    search_fields = ['id', 'slug']
-
-
-class CheckoutViewSet(viewsets.ModelViewSet):
-    queryset = Checkout.objects.all()
-    serializer_class = CheckoutSerializer
-    permission_classes = [AllowAny]
-    filter_backends = [SearchFilter]
-    search_fields = ['id', 'slug']
-
-
 class SiteSettingsViewSet(viewsets.ModelViewSet):
     queryset = SiteSettings.objects.all()
     serializer_class = SiteSettingsSerializers
@@ -168,68 +45,3 @@ class PhoneSiteSettingsViewSet(viewsets.ModelViewSet):
     queryset = SiteSettings.objects.all()
     serializer_class = PhoneSiteSettingsSerializers
     permission_classes = [AllowAny]
-
-
-@api_view(['GET'])
-def selling_status(request):
-    """
-    A view that returns the selling status for the current day.
-
-    The selling status includes the number of checkouts, the total sum of all checkouts,
-    and the last 10 checkouts sorted by creation date.
-    """
-    today = timezone.now().date()
-    checkout_items = Checkout.objects.filter(Q(PAY_STATUS=True) | Q(NAXT_STATUS=True), created_at__date=today)
-
-    checkout_items_total = checkout_items.aggregate(total_price=Sum(Cast('cart__total', output_field=DecimalField())))['total_price'] or 0
-
-    checkout_list = checkout_items.order_by('-created_at')[:10]
-
-    data = {
-        'checkout_count': checkout_items.aggregate(total=Sum('cart__quantity')).get('total') or 0,
-        'total_sum': checkout_items_total,
-        'checkout_list': checkout_list.values()
-    }
-
-    serializer = SellingStatusSerializer(data)
-
-    return Response(serializer.data)
-
-
-class LoginView(TokenObtainPairView):
-    serializer_class = LoginSerializer
-
-
-class ChangePasswordView(APIView):
-    permission_classes = (IsAuthenticated, IsAdminUser)
-    serializer_class = ChangePasswordSerializer
-
-    def put(self, request, *args, **kwargs):
-        serializer = self.serializer_class(data=request.data, context={'request': request})
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response({'detail': _('Password changed successfully.')})
-
-
-class ForgetPasswordView(APIView):
-    serializer_class = ForgetPasswordSerializer
-
-    def post(self, request, *args, **kwargs):
-        serializer = self.serializer_class(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        try:
-            user = User.objects.get(phone_number=serializer.validated_data['phone_number'])
-        except User.DoesNotExist:
-            msg = _('User with provided phone number does not exist.')
-            raise serializers.ValidationError(msg, code='authorization')
-        new_password = User.objects.make_random_password()
-        user.set_password(new_password)
-        user.save()
-        send_mail(
-            _('Your new password'),
-            _('Your new password is: ') + new_password,
-            settings.DEFAULT_FROM_EMAIL,
-            [user.email],
-            fail_silently=False,
-        )
-        return Response({'detail': _('Your new password has been sent to your email.')})
